878d78cb534e02cab40affb731017bde9ed63130
fauzandp <fauzan27dp@gmail.com>
2023-05-17 15:57:22 +0700

    Project28 | Biometric and Keychain App create UI add code to adjust TextView while keyboard appear and disappear

--- Project28/ViewController.swift
+++ Project28/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var secret: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Nothing to see here"
+        
+        let notification = NotificationCenter.default
+        notification.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notification.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
     }
+    
+    @IBAction func authenticateTapped(_ sender: UIButton) {
+     
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardEndFrame, from: view.window)
 
-
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            secret.contentInset = .zero
+        } else {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        
+        secret.scrollIndicatorInsets = secret.contentInset
+        
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
+    }
 }
 
